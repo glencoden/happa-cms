91a9d5e1a44ee3409cf49bbaab644bfa19795c47
glen coden <simon.der.meyer@gmail.com>
2022-10-25 13:00:52 +0200

    concat with schema

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,10 +1,12 @@
 import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 import menu from './menu'
+import dayMenu from './day-menu'
 
 export default createSchema({
     name: 'default',
     types: schemaTypes.concat([
         menu,
+        dayMenu,
     ]),
 })
