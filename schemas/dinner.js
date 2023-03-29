783ce088a9a2eec275214152eaf01a2c5f4c9516
glen coden <info@simonmeyer.de>
2023-03-30 00:44:37 +0200

    add rsvp link

---
+++ schemas/dinner.js
@@ -0,0 +1,12 @@
+export default {
+    title: 'Dinner Events',
+    name: 'dinner',
+    type: 'document',
+    fields: [
+        {
+            title: 'Rsvp Link',
+            name: 'rsvpLink',
+            type: 'string',
+        },
+    ],
+}
--- schemas/schema.js
+++ schemas/schema.js
@@ -3,6 +3,7 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 import localizedString from './localizedString'
 import localizedBlock from './localizedBlock'
 import menu from './menu'
+import dinner from './dinner'
 
 export default createSchema({
     name: 'default',
@@ -10,5 +11,6 @@ export default createSchema({
         localizedString,
         localizedBlock,
         menu,
+        dinner
     ]),
 })
